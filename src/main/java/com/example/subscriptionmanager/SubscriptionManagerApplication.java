bf7135deb9d6e113a0fb11c4fb30be5dd72a9a62
vteeev <piotr.nie00@gmail.com>
2026-01-08 21:52:26 +0100

    Schema of infrastructure

---
+++ src/main/java/com/example/subscriptionmanager/SubscriptionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/subscriptionmanager/SubscriptionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
